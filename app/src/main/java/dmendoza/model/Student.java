a6a5783018029c15f65d3b6b8afc872ba5e71599
David Mendoza Ita <david.isaac.iker@hotmail.com>
2022-05-22 01:49:50 -0500

    fix: quitar abstracción de Person

--- app/src/main/java/dmendoza/model/Student.java
+++ app/src/main/java/dmendoza/model/Student.java
@@ -2,11 +2,25 @@ package dmendoza.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "student")
-public class Student extends Person {
+public class Student {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
+   private Integer id;
+   
+   @Column(name = "name")
+   private String name;
+
+   @Column(name = "lastname")
+   private String lastname;
 
    @Column(name = "code")
    private String code;
@@ -16,6 +30,30 @@ public class Student extends Person {
 
    @Column(name = "profile_picture")
    private String profilePicture;
+
+   public Integer getId() {
+      return id;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getLastname() {
+      return lastname;
+   }
+
+   public void setLastname(String lastname) {
+      this.lastname = lastname;
+   }
 
    public String getCode() {
       return code;
